3004d42986c451a6eb2e48f1571f55f629ca6a5c
Huang Zhimin <zm-huang@126.com>
2025-04-26 23:14:06 +0800

    改进 autoconf 交叉编译处理，及重构
    1 jgb-build
    - export JGB_BUILD_BUILD_ARCH 等
    - 包含项目 options 文件
    
    2 jgb-src
    - 支持跳过打补丁
    
    3 add test.sh, setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+PATH=$PWD:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+./jgb-rebuild-debug
---
+++ test/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+export JGB_BUILD_DEBUG=1
+rm arm64 x86_64 -rf
+mkdir arm64 x86_64
+for arch in arm64 x86_64;
+do
+	cd $arch
+        for pkg in libpcap-1.10.5 tcpdump-4.99.5 traceroute-2.1.6 openssl-3.0.16 libwebsockets-v4.3.5 ;
+	do
+		jgb-src -R $pkg
+		cd $pkg
+		jgb-build -Ri -p $arch
+		cd ..
+	done
+	cd ..
+done
