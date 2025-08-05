3c75b978c95a321965ca806d21ae6611ff87f9ed
黄志敏 <huangzhimin@zohetec.com>
2025-08-05 09:09:22 +0800

    支持从任意目录执行

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
+#set -x
+
+script_full_path=$(realpath "$0")
+d=$(dirname $script_full_path)
+cd $d
 PATH=$PWD:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ./jgb-rebuild-debug
