b2b8b1e7f60ca738ee621e13019ae82ee2b71264
Huang Zhimin <zm-huang@126.com>
2025-10-18 15:08:36 +0800

    add Debug build test

--- test/test.sh
+++ test/test.sh
@@ -5,13 +5,17 @@ rm arm64 x86_64 -rf
 mkdir arm64 x86_64
 for arch in arm64 x86_64;
 do
+    for type_ in Debug Release;
+      do
 	cd $arch
-        for pkg in libpcap-1.10.5 tcpdump-4.99.5 traceroute-2.1.6 openssl-3.0.16 libwebsockets-4.3.5 chrony-4.6.1 ;
+        for pkg in openssl-3.0.16 libpcap-1.10.5 tcpdump-4.99.5 traceroute-2.1.6 libwebsockets-4.3.5 chrony-4.6.1 ;
 	do
 		jgb-src -R $pkg
 		cd $pkg
-		jgb-build -RiL -p $arch
+		jgb-build -RiL -p $arch -t$type_
 		cd ..
 	done
 	cd ..
+      done
 done
+echo 完成
